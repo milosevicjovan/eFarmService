2d1ea2afe497a878a79b4d5319fcace95a0c0cca
Jovan <milosevic.jovan@uns.ac.rs>
2020-12-25 08:27:28 +0100

    added products controller

---
+++ eFarmService/Controllers/ProductsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using eFarmDataAccess;
+using System.Web.Http.Description;
+using eFarmService.Models;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace eFarmService.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        [ResponseType(typeof(ProductsDto))]
+        [Route("api/products/{producerId}")]
+        public async Task<IHttpActionResult> Get(int producerId)
+        {
+            using (eFarmDataEntities entities = new eFarmDataEntities())
+            {
+                if (producerId < 1) 
+                {
+                    return NotFound();
+                }
+
+                var products = await entities.Products.Include(p => p.ProductTypes.ProductCategories).Where(p => p.ProducerId == producerId).Select(p =>
+                                new ProductsDto()
+                                {
+                                    Id = p.Id,
+                                    Category = p.ProductTypes.ProductCategories.Category,
+                                    Product = p.ProductTypes.Name,
+                                    Quantity = (decimal)p.Quantity,
+                                    Price = (decimal)p.Price
+                                }).ToListAsync();
+
+                if (products == null)
+                {
+                    return NotFound();
+                }
+                return Ok(products);
+            }
+        }
+    }
+}
---
+++ eFarmService/Models/Products.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eFarmService.Models
+{
+    class ProductsDto
+    {
+        public int Id { get; set; }
+        public string Category { get; set; }
+        public string Product { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
