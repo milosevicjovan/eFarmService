6cffdad2108b24275942d61db874aee504bfa208
Jovan <milosevic.jovan@uns.ac.rs>
2021-01-25 00:39:39 +0100

    added ProductDto

---
+++ eFarmService/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eFarmService.Models
+{
+    class ProductDto
+    {
+        public int Id { get; set; }
+        public string Category { get; set; }
+        public string Product { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string Producer { get; set; }
+    }
+}
